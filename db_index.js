c0958a4172c4a330e852ce295fa19c38474bf05c
shabbyjoon <ShabnamMokhtarani@Shabnams-MacBook-Pro.local>
2018-09-11 21:10:26 -0700

    generates random number for artistID

--- db_index.js
+++ db_index.js
@@ -6,13 +6,18 @@ let connection = mysql.createConnection({
     password : 'password',
     database : 'artists'
 });
-/*
+
 //randomly generate an artist id for current artist
+
+let randomId = Math.floor(Math.random() * Math.floor(100));
+
+
 const getRelatedArtists = function(currentArtist, getArtist) {
-    connection.query(`select artist_name, artistid, listeners from artist where artistid in (select related_artist_id from relatedartists where main_artist_id = (select artistid from artist where artist_id = "${currentArtist}"))`, function(error, result, fields) {
+    connection.query(`select artist_name, artistid, listeners from artist where artistid in (select related_artist_id from relatedartists where main_artist_id = (select artistid from artist where artist_id = ${randomID}))`, function(error, result, fields) {
         if (error) {
             getRelatedArtists(error);
         } else {
+
             console.log(result);
             getRelatedArtists(null, result);
         }
@@ -20,8 +25,8 @@ const getRelatedArtists = function(currentArtist, getArtist) {
 };
 //select * from relatedartists where main_artist_id = 10;+-----+-------------------+----------------+
 // select artist_name, artistid, listeners from artist  where artistid in (select related_artist_id from relatedartists where main_artist_id= (select artistid from artist where artist_name= 'Ms. Rick Leuschke'));
-*/
-//module.exports.getRelatedArtists = getRelatedArtists;
+
+module.exports.getRelatedArtists = getRelatedArtists;
 
 
 
