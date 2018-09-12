ae1f40502a4bef6996a81b8f0f5123e283f4561b
shabbyjoon <ShabnamMokhtarani@Shabnams-MacBook-Pro.local>
2018-09-11 21:10:57 -0700

    created empty component and rendered

--- client/src/relatedArtist.jsx
+++ client/src/relatedArtist.jsx
@@ -1,3 +1,14 @@
-//contains artists container and button
+//contains artists container and button, displays each entry
 import React from 'react';
 import ReactDOM from 'react-dom'
+
+const relatedArtist = (props) =>(
+       
+            <div>
+              <div>'hello from hello</div>
+
+               {/* //map each related artist entry */}
+              
+            </div>
+    )
+export default relatedArtist;
