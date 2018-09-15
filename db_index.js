d8ed64c7efcae91b65dc6d3cf61ab51c2e2da6c9
shabbyjoon <ShabnamMokhtarani@Shabnams-MacBook-Pro.local>
2018-09-15 02:20:32 -0700

    adds popularSong to db query

--- db_index.js
+++ db_index.js
@@ -9,7 +9,7 @@ let connection = mysql.createConnection({
 
 const getRelatedArtists = function(showArtist) {
     let randomId = Math.floor(Math.random() * Math.floor(100));
-    connection.query(`select artist_name, artistid, listeners, artist_image from artist where artistid in (select related_artist_id from relatedartists where main_artist_id = (select artistid from artist where artistid = 10))`, function(error, result){
+    connection.query(`select artist_name, artistid, listeners, artist_image, popularSong from artist where artistid in (select related_artist_id from relatedartists where main_artist_id = (select artistid from artist where artistid = 10))`, function(error, result){
         if (error) {
             console.log('db query error');
             showArtist(error, null);
