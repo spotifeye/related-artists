d7c70841298478209e914f692e32f8ca4b67a0be
shabbyjoon <ShabnamMokhtarani@Shabnams-MacBook-Pro.local>
2018-09-15 01:41:33 -0700

    gets data via props and renders artist image, name, and listeners

--- client/src/relatedArtistEntry.jsx
+++ client/src/relatedArtistEntry.jsx
@@ -1,43 +1,20 @@
-//displays a random artist
-//state: followers (on hover toggle)
+
 import React from 'react';
 
+const relatedArtistEntry = (props) => {
+   
 
-
-class relatedArtistEntry extends React.component {
-    constructor(props) {
-        super(props);
-
-        this.state = {
-           toggleListeners: false,
-        }
-        this.displayListenersHandler = this.displayListenersHandler.bind(this);
-    }
-    
-    //toggles listeners display when user hovers
-    displayListenersHandler() {
-            this.setState(prevState => ({
-                toggleListeners: !prevState.toggleListeners
-            }));
-    }
-
-    render() {
-        return(        
-          <div>{props.artist.artist_name}</div>
-          <div>{props.artist.artist_image}</div>
-          <div 
+    return(
+          <div>
+               <div><img src={props.artist.artist_image}></img></div>
+               <div>{props.artist.artist_name}</div> 
+               <div>{props.artist.listeners}</div>
+          </div>
+        //   <div>{props.artist.artist_image}</div>
+          
         //   onMouseEnter={this.displayListenersHandler()}
         //   onMouseLeave={this.displayListenersHandler()}
-
-
-          >
-
-              {/* toggles to true when mouse is on listeners div */}
-              {/* toggles to false when mouse is off listeners div */}
-          </div>
-          )
-
-    }
-
-
+    )
 }
+
+export default relatedArtistEntry;
