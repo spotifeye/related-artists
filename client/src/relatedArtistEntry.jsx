4eb8ba68a8a50917b09fd78ac89def0d12e4f80d
shabbyjoon <ShabnamMokhtarani@Shabnams-MacBook-Pro.local>
2018-09-15 02:47:26 -0700

    adds basic onClick functionality to display each artist's popular song

--- client/src/relatedArtistEntry.jsx
+++ client/src/relatedArtistEntry.jsx
@@ -1,20 +1,35 @@
 
 import React from 'react';
+import PopularSong from './popularSong.jsx';
 
-const relatedArtistEntry = (props) => {
-   
+class relatedArtistEntry extends React.Component {
+   constructor(props) {
+       super(props);
+       this.state = {
+           isPopupHidden: true,
+           isListenersHidden: true
+       }
+       this.togglePopup = this.togglePopup.bind(this);
+   }
+   togglePopup () {
+       this.setState({
+           isPopupHidden: !this.state.isPopupHidden
+       })
+   }
 
+   render() {
     return(
-          <div>
-               <div><img src={props.artist.artist_image}></img></div>
-               <div>{props.artist.artist_name}</div> 
-               <div>{props.artist.listeners}</div>
+          <div onClick={this.togglePopup}>
+               <div><img src={this.props.artist.artist_image}></img></div>
+               <div>{this.props.artist.artist_name}</div> 
+               <div>{this.props.artist.listeners}</div>
+                  <div> {!this.state.isPopupHidden && <PopularSong artist={this.props.artist}/>}</div>
           </div>
-        //   <div>{props.artist.artist_image}</div>
           
         //   onMouseEnter={this.displayListenersHandler()}
         //   onMouseLeave={this.displayListenersHandler()}
     )
+   }
 }
 
 export default relatedArtistEntry;
