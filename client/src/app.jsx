1b8a56697b49fa249a22ef9ecb463c8218311962
shabbyjoon <ShabnamMokhtarani@Shabnams-MacBook-Pro.local>
2018-09-18 00:10:37 -0700

    adds toggle feature to more artists button

--- client/src/app.jsx
+++ client/src/app.jsx
@@ -6,14 +6,16 @@ import styles from './styles/relatedArtist.css'
 
 const axios = require('axios');
 var allArtists;
+var firstFour;
 class App extends React.Component {
    constructor(props) {
        super(props);
        this.state = {  
          relatedArtists : [],
+         toggleArtistList : false,
          firstFourArtists : []
        }
-
+       this.lessArtists = this.lessArtists.bind(this);
        this.getRelatedArtists = this.getRelatedArtists.bind(this);
        this.moreArtistsHandleClick = this.moreArtistsHandleClick.bind(this);
    }
@@ -23,7 +25,7 @@ class App extends React.Component {
     axios.get('http://localhost:3002/relatedArtists/id/artist')
       .then((response) => {
         allArtists = response.data;
-        let firstFour = [];
+        firstFour = [];
         for (let i = 0; i < response.data.length; i++) {
             if( i < 5 )
             firstFour.push(response.data[i])
@@ -39,11 +41,24 @@ class App extends React.Component {
      });
    }
 
+   lessArtists() {
+    this.setState({
+      toggleArtistList: !this.state.toggleArtistList
+    });
+   }
+
    moreArtistsHandleClick() {
+       this.lessArtists();
+    if(this.state.toggleArtistList === false){
       this.setState({
         relatedArtists : allArtists
       })
       console.log('i was clicked')
+    } else {
+        this.setState({
+            relatedArtists : firstFour
+        })
+    }
    }
 
    componentDidMount() {
@@ -55,15 +70,18 @@ class App extends React.Component {
 
         return (
         <div>
+            <div>
             <div styleName='RAContainer'>
             <h1>Related Artists</h1>
               <div id="panel panel-default">
                 <div id="panel-body"><RelatedArtists relatedArtists={this.state.relatedArtists}/></div>     
                 <div styleName='RAButton' class="panel-footer">
-                <button onClick={this.moreArtistsHandleClick}>More Artists</button>
+                <button onClick={this.moreArtistsHandleClick}> More Artists</button>
               </div>
+
               </div>
             </div>
+        </div>
         </div>
         );
     }
