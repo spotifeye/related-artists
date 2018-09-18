e8548311931e4777f1e804743d4a3d9c110277bd
shabbyjoon <ShabnamMokhtarani@Shabnams-MacBook-Pro.local>
2018-09-18 11:45:51 -0700

    exports to window

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom'
-import App from './app.jsx';
+import RAApp from './app.jsx';
 
 
-ReactDOM.render(<App />, document.getElementById('app'));
+ReactDOM.render(<RAApp />, document.getElementById('raapp'));
+
+window.RAApp = RAApp;
