229fb43dfb5b5d383551ea13eeace25f4735ce1e
shabbyjoon <ShabnamMokhtarani@Shabnams-MacBook-Pro.local>
2018-09-19 18:24:55 -0700

    fixed styling, cleaned up code

--- client/src/listeners.jsx
+++ client/src/listeners.jsx
@@ -3,13 +3,8 @@ import styles from './styles/relatedArtist.css';
 import CSSModules from 'react-css-modules';
 
 const listeners = props => (
-  <div>
-
-    <div className="RAListeners">
-      {props.artist.listeners
-        .toString ()
-        .replace (/\B(?=(\d{3})+(?!\d))/g, ',')}
-    </div>
+  <div className="RAListeners">
+    {props.artist.listeners.toString ().replace (/\B(?=(\d{3})+(?!\d))/g, ',')}
   </div>
 );
 
