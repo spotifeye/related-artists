74c335ffc189417912bae2179d7df4cc5fb70e93
shabbyjoon <ShabnamMokhtarani@Shabnams-MacBook-Pro.local>
2018-09-20 18:30:54 -0700

    adds escaping for query values

--- db_index.js
+++ db_index.js
@@ -7,18 +7,19 @@ let connection = mysql.createConnection ({
 });
 
 const getRelatedArtists = function (id, showArtist) {
-  connection.query (
-    `select artist_name, artistid, listeners, artist_image, popularSong from artist where artistid in (select related_artist_id from relatedartists where main_artist_id = (select artistid from artist where artistid = ${id}))`,
-    function (error, result) {
-      if (error) {
-        console.log ('db query error');
-        showArtist (error, null);
-      } else {
-        console.log ('db query success');
-        showArtist (null, result);
-      }
+  let sqlQuery =
+    `select artist_name, artistid, listeners, artist_image, popularSong from artist where artistid in (select related_artist_id from relatedartists where main_artist_id = (select artistid from artist where artistid =` +
+    connection.escape (id) +
+    `))`;
+  connection.query (sqlQuery, function (error, result) {
+    if (error) {
+      console.log ('db query error');
+      showArtist (error, null);
+    } else {
+      console.log ('db query success');
+      showArtist (null, result);
     }
-  );
+  });
 };
 
 module.exports.getRelatedArtists = getRelatedArtists;
