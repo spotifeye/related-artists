a477f8e8007c411949a457d1050282904a394273
Chris Larson <c.lars701@gmail.com>
2018-10-08 13:23:09 -0700

    add license key placeholder

--- newrelic.js
+++ newrelic.js
@@ -14,6 +14,7 @@ exports.config = {
   /**
    * Your New Relic license key.
    */
+  license_key: 'override',
   logging: {
     /**
      * Level at which to log. 'trace' is most useful to New Relic when diagnosing
