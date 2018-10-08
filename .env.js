8967947b1aeb0905f54fe8d24575cb93aa70b3bc
Chris Larson <c.lars701@gmail.com>
2018-10-08 13:00:16 -0700

    move new relic key to env var

--- .env.js
+++ .env.js
@@ -4,5 +4,4 @@ module.exports = {
   DB_PW: process.env.DB_PW || 'password',
   PORT: process.env.PORT || 3002,
   REDIS_PORT: process.env.REDIS_PORT || 6379,
-  NEW_RELIC_KEY: process.env.NEW_RELIC_KEY || 'placeholder',
 };
